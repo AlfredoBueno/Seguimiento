1e64c27fe59d18e39988cf5b1d73e94b4975d404
AlfredoBueno <alfredo9805@gmail.com>
2021-04-21 12:36:08 -0500

    initial commit

---
+++ SeguimientoAlumnosEgresados2/Controllers/carrerasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SeguimientoAlumnosEgresados2.Models;
+
+namespace SeguimientoAlumnosEgresados2.Controllers
+{
+    public class carrerasController : Controller
+    {
+        private seguimiento2Entities db = new seguimiento2Entities();
+
+        // GET: carreras
+        public ActionResult Index()
+        {
+            return View(db.carreras.ToList());
+        }
+
+        // GET: carreras/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            carrera carrera = db.carreras.Find(id);
+            if (carrera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carrera);
+        }
+
+        // GET: carreras/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: carreras/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idCarrera,nombreCarrera")] carrera carrera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.carreras.Add(carrera);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(carrera);
+        }
+
+        // GET: carreras/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            carrera carrera = db.carreras.Find(id);
+            if (carrera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carrera);
+        }
+
+        // POST: carreras/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idCarrera,nombreCarrera")] carrera carrera)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(carrera).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(carrera);
+        }
+
+        // GET: carreras/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            carrera carrera = db.carreras.Find(id);
+            if (carrera == null)
+            {
+                return HttpNotFound();
+            }
+            return View(carrera);
+        }
+
+        // POST: carreras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            carrera carrera = db.carreras.Find(id);
+            db.carreras.Remove(carrera);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SeguimientoAlumnosEgresados2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeguimientoAlumnosEgresados2.MvcApplication" Language="C#" %>
